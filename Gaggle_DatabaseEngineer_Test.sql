e385d25835366e0d937bb5b7f56510aaf67bcc6b
ShaqDeezle <86494490+ShaqDeezle@users.noreply.github.com>
2021-11-08 06:14:46 -0500

    Update Gaggle_DatabaseEngineer_Test.sql

--- Gaggle_DatabaseEngineer_Test.sql
+++ Gaggle_DatabaseEngineer_Test.sql
@@ -2,13 +2,13 @@
 
 SELECT CONCAT(first_name, " ", last_name) AS "Full Name" FROM customer
 WHERE UPPER(first_name) = (SELECT first_name FROM actor
-						   WHERE actor_id = 8)
+			   WHERE actor_id = 8)
 
 UNION
 
 SELECT CONCAT(first_name, " ", last_name) FROM actor
 WHERE actor_id != 8 AND UPPER(first_name) = (SELECT first_name FROM actor
-									         WHERE actor_id = 8);
+					     WHERE actor_id = 8);
 
 
 
@@ -19,7 +19,7 @@ ALTER TABLE actor ADD (UUID BINARY(16) UNIQUE);
 
 ALTER TABLE customer ADD (UUID BINARY(16) UNIQUE);
 
-	-- Assigning UUID() as DEFAULT value won't work because it does not guarantee UNIQUE values
+	-- Assigning UUID() as a DEFAULT value won't work because it does not guarantee UNIQUE values.
 
 
 	/*
@@ -35,7 +35,7 @@ ALTER TABLE customer ADD (UUID BINARY(16) UNIQUE);
 	Another method you can do instead of following the 4 steps above is to execute: 
 	SET SQL_SAFE_UPDATES = 0;
 
-	That should turn off the safety precautions, but make sure to set it back to 1 because it is a good safety precaustion to have.
+	That should turn off the safety precautions, but make sure to set it back to 1 because it is a good safety precaution.
 
 	*/
 
@@ -48,14 +48,14 @@ SET UUID = (UUID_TO_BIN(UUID()));
 
 	-- The UUID_TO_BIN() function converts a UUID from a human-readable format (VARCHAR) into a compact format (BINARY) format for storing.
 
-	-- Your column is of type binary(16) which means UUID data is implicitly converted to binary. using UUID_TO_BIN is not needed.
+	-- Your column is of the datatype, binary(16), which means UUID data is implicitly converted to binary. Therefore, using UUID_TO_BIN is not needed.
 
 
 SELECT actor_id, CONCAT(first_name, " ", last_name) AS "Full Name", last_update, BIN_TO_UUID(uuid) as "UUID" FROM actor;
 
 SELECT customer_id, store_id, CONCAT(first_name, " ", last_name) AS "Full Name", email, address_id, active, create_date, last_update, BIN_TO_UUID(uuid) as "UUID" FROM customer;
 
-	-- the BIN_TO_UUID() function converts UUID from the compact format (BINARY)to human-readable format (VARCHAR) for displaying.
+	-- The BIN_TO_UUID() function converts UUID from the compact format (BINARY) to human-readable format (VARCHAR) for displaying.
 
 
 DELIMITER #
@@ -84,7 +84,7 @@ DELIMITER ;
 -- Question 3)
 
 
-	-- There's a few ways to go about doing this. You can create a JOIN to display the data:
+	-- There are a few ways to go about doing this. First, you can create a JOIN to display the data:
 
 SELECT CONCAT(cu.first_name, " ", cu.last_name) AS "Full Name", cu.email, cu.address_id FROM customer cu
 JOIN address a
@@ -104,17 +104,17 @@ where address_id in (select address_id from address a where city_id in (
 select city_id from city c where country_id in (select country_id from
 country ct where country = 'Canada'))));
 
-	-- After executing this in MySQL Workbench, the "EXPLAIN" suggessted that I use an index on the "country" table to improve the query.
+	-- After executing this in MySQL Workbench, the "EXPLAIN" suggested using an index on the "country" table to improve the query.
 
 
 SHOW INDEX FROM country FROM sakila;
 
-	-- I ran the command above to see how many indexes and what each of those indexes were on the country table
+	-- I ran the command above to see how many (indexes) and what each of those indexes was on the country table.
 
 
 CREATE INDEX country_idx ON country (country);
 
-	-- I created an index on the "country" column based on the "EXPLAIN", and wanting to change the "type" for the country table from "ALL" to "ref". 
+	-- I created an index on the "country" column based on the "EXPLAIN", and wanted to change the "type" for the country table from "ALL" to "ref".
 
 
 
@@ -122,13 +122,20 @@ CREATE INDEX country_idx ON country (country);
 
 	/*
 	
-	So to answer this question, you can create your own functions, however, starting from MySQL 8 onwards, there are internal/data dictionary functions that were created called "BIN_TO_UUID" and "UUID_TO_BIN". These functions convert the hex, which is a function in MySQL that is used to return an equivalent hexadecimal string value of a string or numeric input, to binary by using unhex. UNHEX function in MySQL is used to convert the Hexadecimal number into the bytes represented by the Number. The value returned by it is a binary string, and vice versa, converting a binary string to a hex, using the HEX function.
+	MySQL 8 onwards has new internal/data dictionary functions called "BIN_TO_UUID" and "UUID_TO_BIN". These functions convert the hex, which is a function in MySQL that returns an equivalent hexadecimal string value of a string or numeric input, to binary by using unhex. UNHEX function in MySQL is used to convert the Hexadecimal number into the bytes represented by the number. The value it returns is a binary string, and vice versa, converting it to a hex using the HEX function.
 	
 	
 	However, if you do not have MySQL 8+ or you want to create your own "BIN_TO_UUID" and "UUID_TO_BIN" functions, then you can use the code below:
 	
 	
-	Polyfill for "BIN_TO_UUID" and "UUID_TO_BIN" for MySQL with the swap_flag parameter.
+	A substitute fill in code for "BIN_TO_UUID" and "UUID_TO_BIN" for MySQL with the swap_flag parameter.
+	
+	
+		-- If swap_flag is 0, the two-argument form is equivalent to the one-argument form. Therefore, the string result is in the same order as the binary argument.
+
+		
+		-- If swap_flag is 1, the UUID value is assumed to have its time-low and time-high parts swapped. These parts are swapped back to their original position in the result value.
+
 	
 	*/
 
@@ -164,4 +171,3 @@ BEGIN
 END$$
 
 DELIMITER ;
-
